ffdded2d09a5c9ebe5b32544af6479fe09233dbd
jjb169 <59629141+jjb169@users.noreply.github.com>
2021-07-21 12:15:09 -0400

    Upload empty source code

---
+++ Ring_It.c
@@ -0,0 +1,18 @@
+//Ring It! Source code
+//Luca Mahoney
+//Zach Hartman
+//James Bickerstaff
+
+
+
+void setup() 
+{
+	//set pin modes  like below
+	pinMode(X, OUTPUT);  
+	pinMode(Y, INPUT);    
+}
+
+void loop() 
+{
+
+}
